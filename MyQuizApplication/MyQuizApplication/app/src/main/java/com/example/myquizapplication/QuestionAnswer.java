0967fb42d187174b8c9dfdc3e7f7e6aff0af1720
JuanRojasL <112908543+JuanRojasL@users.noreply.github.com>
2022-11-23 12:05:17 -0500

    Add files via upload

---
+++ MyQuizApplication/MyQuizApplication/app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,33 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question [] = {
+
+            "Which company owns the android?",
+            "Which one is not the programming language",
+            "Where are you watching this video?",
+            "Which company owns Apple?"
+    };
+
+    public static String choices [] [] = {
+
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"}
+
+
+    };
+
+    public static String correctAnswers [] = {
+
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple"
+
+    };
+
+
+}
